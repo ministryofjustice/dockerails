3aebb3a4c32792bff1648fe00290232ba94a4b45
Ash Berlin <ash_github@firemirror.com>
2015-09-17 12:24:50 +0100

    Disable ActiveRecord::QueryCache so that we can catch database errors in the controler
    With QueryCache enabled the exception is thrown outside the controller
    so we can't catch it. Disabling it means we get to handle it how we want

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @previous_access = Access.last
 
     Access.create!(code: SecureRandom.urlsafe_base64(20))
+  rescue => e
+      @err = e
   end
 end
